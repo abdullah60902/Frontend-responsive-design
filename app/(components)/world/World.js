2630df74245a457863ac4d42dd12031ba45696e6
abdullah60902 <butt60902@gmail.com>
2024-07-17 14:32:34 +0500

    Normalize line endings

--- app/(components)/world/World.js
+++ app/(components)/world/World.js
@@ -7,7 +7,7 @@ export default function World() {
         <section className="about-us">
         <div className="row">
             <div className="about-col">
-                <h1>We are the world's largest University</h1>
+                <h1>We are the world largest University</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore cumque in accusantium molestias maiores fuga vitae eos, ducimus officiis repudiandae voluptatem error. Laboriosam, numquam blanditiis aspernatur, nobis tempora consequatur id aliquam asperiores voluptatum iste animi voluptatem fugiat totam excepturi dolor.</p>
                 <a href="#" className="hero_btn btn">EXPLORE NOW</a>
             </div>
